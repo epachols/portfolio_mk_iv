f264b94f200898b590b1f86f297ef490e4d57b0e
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-09-11 08:48:08 -0700

    base color added, will change bootstrap theme later

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header
+        style={{
+          textAlign: `center`,
+          backgroundColor: `darkorchid`,
+          width: `100vw`,
+          // height: `10vh`,
+        }}
+      >
+        <h1>header with stuff man</h1>
       </header>
+      
+      <main style={{backgroundColor: `inherit`}}>
+        <section>intro to self</section>
+        <section>motivations and goals</section>
+        <section>past history/work experience</section>
+      </main>
     </div>
   );
 }
